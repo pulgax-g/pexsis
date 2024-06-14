783146924ca63f6463fe71854254c6444052d3a9
pulgax-g <sjeje2553@gmail.com>
2024-06-14 16:44:05 +0200

    Create pexsis.bat
    the menu eith all tools installed

---
+++ pexsis.bat
@@ -0,0 +1,14 @@
+@echo off
+color 1
+echo   .d``                       uL   ..        .   kk   88P        .   9k 
+echo   @8Ne.   .u        .u     .@88b  @88R    .@8Ned8"    .       .@8Ned8" 
+echo   48888:u@88N    ud8888.  '"Y888k/"*P   .@^88888"   .@88u   .@^88888"  
+echo    `888I  888. :888'8888.    Y888L     x88:  `)8b. ''888E` x88:  `)8b. 
+echo     888I  888I d888 '88%"     8888     8888N=*8888   888E  8888N=*8888 
+echo     888I  888I 8888.4"        `888N     98"    R88   888E   98"    R88 
+echo   uW888L  888' 8888L       .u./"888&     @8Wou 9%    888E    @8Wou 9%    made
+echo  '*88888Nu88P  '8888c. .  d888" Y888*" .888888P`     8888  .888888P`      by
+echo  ~ '88888F`     "88888%   ` "Y   Y"    `   ^"F       R888" `   ^"F      pulgax
+echo     888 ^         "YP'                                ""               
+echo      98                                                                github= pulgax-g
+pause
